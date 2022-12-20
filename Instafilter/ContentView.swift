d19fd0b91c11419a5d288b505ea93324556fe023
arthur <ERHUE@EX.UA>
2022-12-20 13:06:53 +0200

    - add  save button with new ImageSave view

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,8 +6,7 @@
 //
 
 import SwiftUI
-import CoreImage
-import CoreImage.CIFilterBuiltins
+
 
 struct ContentView: View {
     @State private var image: Image?
@@ -22,7 +21,14 @@ struct ContentView: View {
             Button("add image") {
                 isSheetShowing = true
             }
-        }
+            
+            Button("Save image") {
+                guard let inputImage = inputImage else { return }
+                    let imageSaver = ImageSaver()
+                
+                imageSaver.writeToPhotoAlbum(image: inputImage)
+                }
+            }
         .onChange(of: inputImage, perform: { _ in convertImages()})
         .sheet(isPresented: $isSheetShowing) {
             imagePicker(image: $inputImage)
@@ -36,43 +42,7 @@ struct ContentView: View {
     }
     
     
-    func loadImage() {
-        guard let inputImage = UIImage(named: "example") else { return}
-        let beginImage = CIImage(image: inputImage)
-        
-        let context = CIContext()
-        let currentFilter = CIFilter.bloom()
-        currentFilter.inputImage = beginImage
-        
-        
-        let amount = 1.0
-        let inputKeys = currentFilter.inputKeys
-        
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey)
-        }
-        
-        if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(amount * 40, forKey: kCIInputRadiusKey)
-        }
-        
-        if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey)
-        }
-      
-        
-        guard let outputImage = currentFilter.outputImage else { return }
-        
-        if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
-            let uiImage = UIImage(cgImage: cgimg)
-            image = Image(uiImage: uiImage)
-        }
-            
-        
-        
-        
-    }
-    
+  
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ Instafilter/ImageSaver.swift
@@ -0,0 +1,19 @@
+//
+//  ImageSaver.swift
+//  Instafilter
+//
+//  Created by Arthur Sh on 20.12.2022.
+//
+
+import UIKit
+
+class ImageSaver: NSObject {
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+    
+    
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        print("Save completed")
+    }
+}
